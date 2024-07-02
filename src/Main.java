0d714ba92411e80a400609de7ff2da4db59c5ea1
leonovt <leonovt@post.bgu.ac.il>
2024-07-02 15:13:39 +0300

    view 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) {
-        System.out.println("Press any key to continue...");
 
-        try {
-            // Read a single byte/character
-            int ch = System.in.read();
-
-            // Cast the byte to char
-            char c = (char) ch;
-
-            System.out.println("You pressed: " + c);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+      System.out.println("elad cool");
     }
 }
